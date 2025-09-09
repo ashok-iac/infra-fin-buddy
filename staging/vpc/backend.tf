5a2b8dd5961dca5d7a34fa1762e9c4b0f5ae57fe
AshokPabra <ashokpabra1858@gmail.com>
2025-09-09 23:53:04 +0530

    correct the bucket name in staging

--- staging/vpc/backend.tf
+++ staging/vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "infra-fin-buddy-bucket"
+    bucket = "staging-infra-s3-bucket"
     key    = "staging/vpc/terraform.tfstate"
     region = "ap-south-1"
   }
